674cfab836f6b82996194e9a9caa34e2705c1c5d
Tati <83588224+TatiFelizia@users.noreply.github.com>
2024-08-07 15:18:49 -0300

    feat: "Update boot.py"

--- actividades/ej01/boot.py
+++ actividades/ej01/boot.py
@@ -1,1 +1,16 @@
 # Configuracion inicial
+import time
+def do_connect():
+    import network
+    from time import time
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('Cooperadora Alumnos', '')
+        while not sta_if.isconnected():
+            print(".", end = "")
+            sleep(.05)
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
